43d6b7388207da5d734763ac858c887f73a6dc1d
sas10x <kristanraearcino@gmail.com>
2020-05-05 22:16:30 +0800

    finish chapter two

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -28,5 +28,20 @@ namespace API.Controllers
         {
 	        return await _mediator.Send(new Details.Query{Id = id});
         }  
+        [HttpPost]
+        public async Task<ActionResult<Unit>> Create(Create.Command command)
+        {
+            return await _mediator.Send(command);
+        }
+        [HttpPut("[{id}")]
+        public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command)
+        {
+            return await _mediator.Send(command);
+        }
+        [HttpDelete("[{id}")]
+        public async Task<ActionResult<Unit>> Delete(Guid id, Delete.Command command)
+        {
+            return await _mediator.Send(command);
+        }
     }
 }
